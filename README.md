931941a6d061ef1b5eaa4a7827c696385a413b20
Jon Prentice <jon77p@gmail.com>
2019-04-19 11:13:59 -0700

    Initial v1.0 commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SSH Tab Completion
+
+Source the ssh_tools.sh file to have ssh tab-completion setup.
+Based off of defined hosts in the `~/.ssh/config` file!
+This also has a ssh_ip function that, when called with a single argument, will attempt to retrieve the IP address for the specified host from the `~/.ssh/config` file!
---
+++ ssh_tools.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ssh_ip()
+{
+	HOST="$1"
+	if [[ "$HOST" == "" ]]; then
+		return
+	fi
+
+	SEARCH="$(cat ~/.ssh/config | grep "Host $HOST" -A 1)"
+	SPLIT="$(cut -d ' ' -f2 <<<"$SEARCH")"
+	IP="$(tail -n 1 <<<"$SPLIT")"
+	if [[ "$IP" == "" ]]; then
+		return
+	else
+		echo "$IP"
+	fi
+}
+
+_ssh_completions()
+{
+	HOSTS="$(cat ~/.ssh/config | grep 'Host ')"
+	SPLIT="$(cut -d ' ' -f2 <<< "$HOSTS")"
+	COMPREPLY=($(compgen -W "$SPLIT" "${COMP_WORDS[1]}"))
+}
+
+complete -F _ssh_completions ssh
